592aa1ed63e26e8c1be93f26349b11ea76968eee
Manoj Prabaker aka MJ <120574373+manojprabaker@users.noreply.github.com>
2023-04-27 16:49:15 +0530

    add files

---
+++ main.js
@@ -0,0 +1,18 @@
+let shareBtn=document.getElementById("share");
+let instaPost=document.getElementById("insta-post");
+let clsBtn=document.getElementById("cls-btn");
+let shrTray=document.getElementsByClassName("share-tray");
+
+shareBtn.addEventListener("click",function(){
+	this.parentNode.style.display="none"
+	console.log("fsg");
+	shrTray[0].classList.toggle("active");
+
+});
+
+clsBtn.addEventListener("click",function(){
+	let prnt=this.parentNode;
+	prnt.classList.toggle("active");
+	instaPost.style.display="block";
+})
+
